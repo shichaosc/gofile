7d6502b0f664c36c6b4e17895e8a0f307803f0f3
shichao <563405366@qq.com>
2020-05-29 16:06:21 +0800

    修改数据查询修改

--- postgresql/query.go
+++ postgresql/query.go
@@ -1,1 +1,24 @@
 package postgresql
+
+import (
+	"fmt"
+)
+
+func queryBook(bookName string) {
+	err := DB.QueryRow("SELECT * FROM book_book where book_name=$1", bookName)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	//fmt.Printf()
+	//defer rows.Close()
+
+	//for rows.Next() {
+	//	p := new(Book)
+	//	err := rows.Scan(&p.uid, &p.username, &p.departname,&p.Created)
+	//	if err != nil {
+	//		fmt.Println(err)
+	//	}
+	//	fmt.Println(p.uid, p.username, p.departname,p.Created)
+	//}
+}
